48c3a0c4b485d59fb94569ce8c56415693031788
nehhhhha6 <n0370525@gmail.com>
2025-05-14 13:40:48 +0530

    update

--- frontend/src/app/main/page.tsx
+++ frontend/src/app/main/page.tsx
@@ -160,9 +160,9 @@ export default function HomePage() {
               <div className="relative">
                 <h1 className="text-xl font-light tracking-wide text-[#3c4f3d]">
                   <span className="font-normal">EVO</span>
-                  <span className="text-[#de8246]">2</span>
+                  <span className="text-[#87fb5a]">2</span>
                 </h1>
-                <div className="absolute -bottom-1 left-0 h-[2px] w-12 bg-[#de8246]"></div>
+                <div className="absolute -bottom-1 left-0 h-[2px] w-12 text-[#87fb5a]"></div>
               </div>
               <span className="text-sm font-light text-[#3c4f3d]/70">
                 Variant Analysis
